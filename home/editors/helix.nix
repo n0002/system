afa7c9a5470963e6c554f4da2442a033372a3b2a
n <00004444@skiff.com>
2023-01-06 21:34:21 -0500

    helix: config settings

--- home/editors/helix.nix
+++ home/editors/helix.nix
@@ -2,6 +2,14 @@
 {
  programs.helix = {
     enable = true;
+    
+    settings = {
+      theme = "catppucin_mocha";
+      editor = {
+        line-number = "relative";
+        mouse = false;
+        bufferline = "multiple";
+        };
     };
-
+  };
 }
