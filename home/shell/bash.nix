afed6b285d271a8eacb0f7db2092f0ff99ceebd9
n <00004444@skiff.com>
2023-01-08 21:06:43 -0500

    shell: more aliases

--- home/shell/bash.nix
+++ home/shell/bash.nix
@@ -9,11 +9,25 @@
       "ls" = "ls --color=auto";
       "ll" = "ls -lav --ignore=..";   # show long listing of all except ".."
       "l" = "ls -lav --ignore=.?*";   # show long listing but no hidden dotfiles except "."
+
       "dra" = "cd ~/dra-cla && ./dra-cla"; # open dra-cla
       "ani" = "cd ~/ani-cli && ./ani-cli";
+
       "dots" = "cd ~/.dotfiles"; #dotfiles
       "gallery" = "gallery-dl";
+
+      
       ".."  = "cd .. ";
+      "..."  = "cd ../.. ";
+      "...."  = "cd ../../.. ";
+      "....."  = "cd ../../../.. ";
+
+     ncg = "nix-collect-garbage"; 
+     nrb = "nixos-rebuild switch --flake .#laptop";
+    
+    v = "hx";
+    #vim = "hx";
+    #nvim = "hx";
       };
 
       bashrcExtra = ''
