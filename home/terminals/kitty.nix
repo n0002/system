cd1d1417dc1f609a5b9c08307c2f5e1c8f375d22
n <00004444@skiff.com>
2023-01-09 15:40:57 -0500

    kitty: windows, tabs +  keybinds

--- home/terminals/kitty.nix
+++ home/terminals/kitty.nix
@@ -22,10 +22,27 @@
         #tabs
         tab_bar_style = "powerline";
         tab_bar_align = "left";
-       
+        tab_bar_min_tabs = "1";       
         confirm_os_window_close = 0;
         
+        inactive_border_color = "#000000";
+        active_border_color = "#ffccff";
+        window_margin_width = "1";
+        window_margin_padding = "2.0";
    };
+        keybindings = {
+          "ctrl+shift+h" = "previous_tab";
+          "ctrl+shift+l" = "next_tab";
+          "ctrl+shift+t" = "new_tab";
+          "ctrl+shift+w" = "close_tab";
+          "ctrl+shift+r" = "set_tab_title";
+          "alt+l" = "next_window";
+          "alt+h" = "previous_window";
+          "alt+j" = "next_layout";
+          "alt+k" = "previous_layout";
+          "alt+w" = "close_window";
+          
+        };
   };
  };
 }
