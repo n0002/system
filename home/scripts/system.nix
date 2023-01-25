64c06793bf1591b66853f78cda77899dc1ec6a2d
n <00004444@skiff.com>
2023-01-24 20:05:51 -0500

    scripts: add ultrawide

--- home/scripts/system.nix
+++ home/scripts/system.nix
@@ -10,10 +10,14 @@ let
   sleep 2
   nrb
   '';
+  ultrawide = pkgs.writeShellScriptBin "ultrawide" ''
+  wlr-randr --output eDP-1 --off
+  '';
 in
 {
   home.packages = with pkgs; [
     nrb
     ndg
+    ultrawide
   ];
 }
