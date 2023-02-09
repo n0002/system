c9036b9537b16a7d84b07a66e680d8a3d6b2ae8d
n <00004444@skiff.com>
2023-02-08 22:19:24 -0500

    shell: add variables to make screensharing work

--- modules/shell/session.nix
+++ modules/shell/session.nix
@@ -3,17 +3,18 @@
 home = {
 	sessionVariables = {
 	  
-	    EDITOR = "hx";
+	  	EDITOR = "hx";
       BROWSER = "librewolf";
       TERMINAL = "kitty";
-			NIXOS_OZONE_WL = "1";
-      QT_SCALE_FACTOR = "1";
+	  	NIXOS_OZONE_WL = "1";
+      # QT_SCALE_FACTOR = "1";
       MOZ_ENABLE_WAYLAND = "1";
-      SDL_VIDEODRIVER = "wayland";
+      # SDL_VIDEODRIVER = "wayland";
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       
       XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
       XDG_SESSION_DESKTOP = "Hyprland";
       XDG_CACHE_HOME = "\${HOME}/.cache";
       XDG_CONFIG_HOME = "\${HOME}/.config";
