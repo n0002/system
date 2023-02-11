76057cb2a7503e555f0e1237cc78490a0156f62c
n <00004444@skiff.com>
2023-02-11 18:05:19 -0500

    shell: more env vars

--- modules/shell/session.nix
+++ modules/shell/session.nix
@@ -9,7 +9,7 @@ home = {
 	  	NIXOS_OZONE_WL = "1";
       # QT_SCALE_FACTOR = "1";
       MOZ_ENABLE_WAYLAND = "1";
-      # SDL_VIDEODRIVER = "wayland";
+      SDL_VIDEODRIVER = "wayland";
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       
