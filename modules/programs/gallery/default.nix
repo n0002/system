f1b519d4fb97775b7203dc2a6710b7f01e4b3d04
n <00004444@skiff.com>
2023-02-18 20:39:05 -0500

    refactor: prep for snowfall

--- modules/programs/gallery/default.nix
+++ modules/programs/gallery/default.nix
@@ -1,4 +1,18 @@
-{ pkgs, ... }:
+
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+let cfg = config.n.programs.gallery-dl;
+in
 {
-  home.packages = with pkgs; [ gallery-dl];
+  options.n.programs.gallery-dl = with types; {
+    enable = mkBoolOpt true "Whether or not to enable gallery-dl.";
+  };
+
+  config =
+    mkIf cfg.enable { 
+      n.home.extraOptions = {
+        programs.gallery-dl = {
+          enable = true;
+  };
 }
