f71c167825d13b2eae45232c8bb966041f86e1ac
n <00004444@skiff.com>
2023-02-18 20:28:54 -0500

    refactor: prep for snowfall

--- modules/programs/discord/default.nix
+++ modules/programs/discord/default.nix
@@ -1,7 +1,13 @@
- {pkgs, inputs, ...}: {
- 
-   home.packages = [
-    inputs.webcord.packages.${pkgs.system}.default
-    # ...
-  ];
- }
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+let cfg = config.n.programs.discord;
+in
+{
+  options.n.programs.discord = with types; {
+    enable = mkBoolOpt false "Whether or not to enable discord.";
+  };
+
+  config =
+    mkIf cfg.enable { environment.systemPackages = with pkgs; [ discord-canary ]; };
+}
