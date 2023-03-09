f919d92079989d6ba764b815cd4f5d6df8fdae4e
n <00004444@skiff.com>
2023-03-08 19:04:17 -0500

    feat: waybar changes

--- modules/desktop/waybar/default.nix
+++ modules/desktop/waybar/default.nix
@@ -22,32 +22,62 @@ in
                 background-color: rgb(0, 0, 0); 
                 color: rgb(255, 255, 255);
               }
+
+            window#waybar {
+                 background-color: transparent;
+               }
+               window > box {
+                 margin-left: 5px;
+                 margin-right: 5px;
+                 margin-top: 2px;
+                 background-color: rgb(0, 0, 0);
+               }
+            
             #workspaces {
                 background-color: rgb(0, 0, 0); 
+                color: rgb(255, 255, 255);
+                padding-left: 0px;
+                padding-right: 4px;
                 }
+            #memory, #battery, #clock, #window {
+              padding-left: 15px;
+              padding-right: 15px;
+            }
+
       '';
       settings = [{
         "layer" = "top";
         "position" = "top";
         "spacing" = 10; 
         modules-left = [
-        ];
+           "wlr/workspaces" 
+                 ];
         modules-center = [
-          "wlr/workspaces"
-          "battery"
+            # "hyprland/window"
         ];
+
         modules-right = [
           "memory"
+          "clock"
+          "battery"
         ]; 
+
         "wlr/workspaces" = {
           "format" = "{icon}";
+          "on-click" = "activate";
         };
-        
+        "hyprland/window" = {
+          "format" = "{}";
+          "seperate-outputs" = "true";
+          
+        };
         "battery" = {
           };
         "memory" = {
           };
-
+        "clock" = {
+          "format" = "{:%H:%M}";          
+        };
       }];
     };
     };
